a17ea8beec193e15cfb8de654ae0233f1cd2e6b3
Austin Wong <austinwong@umail.ucsb.edu>
2019-01-24 12:21:53 -0800

    arth

--- Arithmetic.asm
+++ Arithmetic.asm
@@ -6,6 +6,38 @@
 main:
 
 	# TODO: Write your code here
+	li	$v0, 5	
+	syscall
+
+	move $t0, $v0
+	syscall
+
+	li	$v0, 5	
+	syscall
+
+	move $t1, $v0
+	syscall
+
+	li	$v0, 5	
+	syscall
+
+	move $t2, $v0
+	syscall
+
+	add $t3, $t0, $t1
+	li $t4 , 1
+	shl $t3, $t4
+	li $t5, 3
+	mult $t5, $t2
+	mflo $t6
+	sub $t7, $t3, $t6
+
+	li $v0, 1
+	move $a0, $t7
+	syscall
+
+
+
 
 exit:
 	# Exit
